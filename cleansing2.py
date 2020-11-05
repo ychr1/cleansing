1cad30f03b864d7b401d168d0b606deb0a3f6d64
namgon <ychr1@naver.com>
2020-11-05 20:20:05 +0900

    up

--- cleansing2.py
+++ cleansing2.py
@@ -1,16 +1,25 @@
 import re
 import pandas as pd
-# from pandas import DataFrame
-# import numpy as np
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
 
-review_data = pd.read_csv('test.csv')
-# df = pd.DataFrame(review_data)
-# print(review_data)
-review_data["review"] = review_data["review"].str.replace("[^ \u3131-\u3163\uac00-\ud7a3]+","") # 한글만 추출
-review_data["review"].str.lstrip()
 
-# review = re.compile("[^ \u3131-\u3163\uac00-\ud7a3]+")
+review_data = pd.read_csv('test.csv',encoding="utf-8-SIG")
 
-review_data.to_csv('test1.csv', mode="w", encoding='utf-8-SIG', index=False)
+# review_data["review"] = review_data["review"].str.replace("[^ \u3131-\u3163\uac00-\ud7a3]+","") # 한글만 추출
+# review_data["review"] = review_data["review"].str.replace("[ㄱ-ㅣ]*","") # ㅋㅋ,ㅎㅎ 삭제
+
+f = open("stopword.txt", "r")
+lines = f.read()
+review_data["review"] = review_data["review"].str.replace(lines,"")
+f.close()
+
+
+    
+
+
+review_data.to_csv('test1.csv', encoding='utf-8-SIG', index=False)
+
+
 
 
