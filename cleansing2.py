6c0903d3e8c1604fe8c798f2ab5a04decfcd8a6a
namgon <ychr1@naver.com>
2020-11-06 19:39:04 +0900

    up

--- cleansing2.py
+++ cleansing2.py
@@ -2,20 +2,25 @@ import re
 import pandas as pd
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
+from konlpy.tag import Okt
 
 
-review_data = pd.read_csv('test.csv',encoding="utf-8-SIG")
+review_data = pd.read_csv('test1.csv',encoding="utf-8-SIG")
 
 # review_data["review"] = review_data["review"].str.replace("[^ \u3131-\u3163\uac00-\ud7a3]+","") # 한글만 추출
 # review_data["review"] = review_data["review"].str.replace("[ㄱ-ㅣ]*","") # ㅋㅋ,ㅎㅎ 삭제
 
-f = open("stopword.txt", "r")
-lines = f.read()
-review_data["review"] = review_data["review"].str.replace(lines,"")
-f.close()
+okt = Okt()
+review_data = okt.morphs(str(review_data), stem=True)
 
 
-    
+
+# 불용어 처리
+# f = open("stopword.txt", "r", encoding="utf-8")
+# lines = f.readlines()
+# review_data["review"] = review_data["review"].str.replace(str(lines),"")
+# review_data.to_csv('test1.csv', encoding='utf-8-SIG', index=False)
+# f.close()
 
 
 review_data.to_csv('test1.csv', encoding='utf-8-SIG', index=False)
