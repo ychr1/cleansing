e7682dfaab3a659561a7a7390f3d0b0a4f981ec0
namgon <ychr1@naver.com>
2020-11-11 21:04:31 +0900

    up

--- cleansing2.py
+++ cleansing2.py
@@ -1,12 +1,10 @@
 import re
 import pandas as pd
 import numpy as np
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
-from konlpy.tag import Okt
 
 
-df = pd.read_csv('test1.csv', sep = ",", encoding="utf-8-SIG")
+
+df = pd.read_csv('test1.csv', encoding="utf-8")
 
 # df["review"] = df["review"].str.replace("[^ㄱ-ㅎ ㅏ-ㅣ 가-힣]","") # 한글만 추출
 # df["review"] = df["review"].str.replace("[ㄱ-ㅣ]*","") # ㅋㅋ,ㅎㅎ 삭제
@@ -21,15 +19,6 @@ df = pd.read_csv('test1.csv', sep = ",", encoding="utf-8-SIG")
 # df = df.dropna()
 # print(df.isnull().sum())
 
-okt = Okt()
-review = []
-for idx, row in df.iterrows():
-    temp_r = []
-    sentence = row["review"]
-    temp_r = okt.morphs(sentence)
-    temp_r = " ".join(temp_r)
-    review.append(temp_r)
 
-df["review"] = review
 df.to_csv('test1.csv', sep=",", encoding="utf-8-SIG", index=False)
 
